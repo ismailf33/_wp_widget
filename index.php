139ff22b3d341931a692b906adb621ac8e8c0b63
alif33 <alifhasan332@gmail.com>
2019-11-13 22:44:04 +0600

    @widget basic stracture

---
+++ index.php
@@ -0,0 +1,10 @@
+<div class="before_widget">
+	<h2>Widget title</h2>
+	<!--  Widget item start -->
+	<div class="widget-item">		
+	<h1>Full Widget item here</h1>
+	</div>
+	<!--  Widget item end -->
+</div>
+
+
